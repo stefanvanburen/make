ec96a9a74f416a613bf8009d40e8f0e9e1c62081
Stefan VanBuren <vanburenstefan@gmail.com>
2018-03-04 10:55:02 -0500

    Update go Makefile

--- go.mk
+++ go.mk
@@ -1,5 +1,18 @@
+# Run unit tests
 test:
 	@go test -v ./...
 
 build:
 	@go build .
+
+# Check for style errors
+lint:
+	@golint .
+
+# Check for syntax errors
+vet:
+	@go vet .
+
+# Clean build artifacts and generated files
+clean:
+	@go clean ./...
