0f7b007c09c6ccf21e561ae2835299d2c894cf05
Stefan VanBuren <vanburenstefan@gmail.com>
2018-03-04 19:44:16 -0500

    Add .PHONY

--- go.mk
+++ go.mk
@@ -1,18 +1,23 @@
 # Run unit tests
+.PHONY: test
 test:
 	@go test -v ./...
 
+.PHONY: build
 build:
 	@go build .
 
 # Check for style errors
+.PHONY: lint
 lint:
 	@golint .
 
 # Check for syntax errors
+.PHONY: vet
 vet:
 	@go vet .
 
 # Clean build artifacts and generated files
+.PHONY: clean
 clean:
 	@go clean ./...
--- python.mk
+++ python.mk
@@ -1,8 +1,11 @@
+.PHONY: install
 install:
 	@pipenv install
 
+.PHONY: install-dev
 install-dev:
 	@pipenv install --dev
 
+.PHONY: test
 test:
 	@pipenv run python -m unittest
