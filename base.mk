6ab1c2868cc8edefd1a9bdc7eb538280f4eb3f9a
Stefan VanBuren <vanburenstefan@gmail.com>
2018-04-04 16:50:40 -0400

    ✨

--- base.mk
+++ base.mk
@@ -1,8 +1,6 @@
 include github.com/svanburen/make/todo.mk
 include github.com/svanburen/make/submodule.mk
 
-.DEFAULT_GOAL := help
-
 # Clean removes all previously used mmake Makefiles
 clean:
 	@rm -rf /usr/local/include/github.com/*
