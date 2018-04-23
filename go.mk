9d4cdeb0954a76901852452304bc88f0c618f6df
Stefan VanBuren <vanburenstefan@gmail.com>
2018-04-23 17:34:56 -0400

    Add generate and metalint targets

--- go.mk
+++ go.mk
@@ -23,3 +23,26 @@ vet:
 clean:
 	@go clean ./...
 .PHONY: clean
+
+generate:
+	@go generate ./...
+.PHONY: generate
+
+# Runs gometalinter on your packages
+# Needs the PKGS variable to be defined
+metalint:
+	gometalinter --vendor --disable-all \
+		--enable=deadcode \
+		--enable=ineffassign \
+		--enable=gosimple \
+		--enable=staticcheck \
+		--enable=gofmt \
+		--enable=goimports \
+		--enable=dupl \
+		--enable=misspell \
+		--enable=errcheck \
+		--enable=vet \
+		--enable=vetshadow \
+		--deadline=1m \
+		$(PKGS)
+.PHONY: metalint
