a93c00a8cc27b98e1761784dc85a950c642a7446
Stefan VanBuren <vanburenstefan@gmail.com>
2020-03-05 20:40:05 -0500

    Remove unused stuff

--- base.mk
+++ base.mk
@@ -1,6 +1,3 @@
-include github.com/svanburen/make/todo.mk
-include github.com/svanburen/make/submodule.mk
-
 # Clean removes all previously used mmake Makefiles
 clean:
 	@rm -rf /usr/local/include/github.com/*
